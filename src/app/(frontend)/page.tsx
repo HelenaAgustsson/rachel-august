63b926e5e938a6b21d7d9fb06f067848c96fee1f
Helena Agustsson <agustssonhelena@gmail.com>
2026-01-30 17:25:32 +0000

    styled h2

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
         <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
             <h1 className="text-4xl font-bold">Rachel August</h1>
             <hr />
-            <h2>Credits</h2>
+            <h2 className="text-2xl font-bold">Credits</h2>
             <Credits />
         </section>
     );
