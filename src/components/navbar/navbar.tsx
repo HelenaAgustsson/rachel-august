282e8f3d7e1e6342972d323c5e062f2bad3dfb78
Helena Agustsson <agustssonhelena@gmail.com>
2026-02-01 14:23:13 +0000

    accessible links

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 export const Navbar = () => {
     return (
         <nav className="flex gap-3 text-red-dark m-5">
-            <Link href="acting" className="flex flex-col justify-end">Acting</Link>
-            <Link href="voiceover" className="flex flex-col justify-end">Voice Over</Link>
-            <Link href="contact" className="flex flex-col justify-end">Contact</Link>
+            <Link href="acting" className="flex flex-col justify-end hover:underline">Acting</Link>
+            <Link href="voiceover" className="flex flex-col justify-end hover:underline">Voice Over</Link>
+            <Link href="contact" className="flex flex-col justify-end hover:underline">Contact</Link>
         </nav>
     )
 }
