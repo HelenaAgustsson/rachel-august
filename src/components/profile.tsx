4280a149de9f3367a9a58770ef38a977b01af984
Helena Agustsson <agustssonhelena@gmail.com>
2026-02-01 20:16:30 +0000

    fix null error

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -6,9 +6,13 @@ export default async function Profile() {
     const { data: profile } = await sanityFetch({ query: PROFILE_QUERY })
 
     return (
-        <section className="md:w-2/3 p-5 bg-orange-100 text-red-dark rounded-md">
-            <h3 className="text-xl font-bold">{profile.title}</h3>
-            <PortableText value={profile.body} />
-        </section>
+        <>
+            {profile ? (
+                <section className="md:w-2/3 p-5 bg-orange-100 text-red-dark rounded-md">
+                    <h3 className="text-xl font-bold">{profile.title}</h3>
+                    <PortableText value={profile.body} />
+                </section>
+            ) : null}
+        </>
     )
 }
