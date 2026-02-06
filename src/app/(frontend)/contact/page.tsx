6bbfb7e3849851d9284d9be8cfa6e99228b94861
Helena Agustsson <agustssonhelena@gmail.com>
2026-02-06 11:18:18 +0000

    set up pages

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,6 +1,13 @@
+import { StandardHero } from "@/components/hero/standard-hero";
+
 const Page = () => {
     return (
-        <div></div>
+        <div>
+            <StandardHero />
+            <main className="w-4/5 md:w-2/3 mx-auto mb-20 p-5 bg-orange-100 text-red-dark rounded-md">
+
+            </main>
+        </div>
     )
 }
 
--- src/app/(frontend)/voiceover/page.tsx
+++ src/app/(frontend)/voiceover/page.tsx
@@ -1,6 +1,14 @@
+import { StandardHero } from "@/components/hero/standard-hero";
+import Credits from "@/components/credits/credits";
+
 const Page = () => {
     return (
-        <div></div>
+        <div>
+            <StandardHero />
+            <main className="w-4/5 md:w-2/3 mx-auto mb-20 p-5 bg-orange-100 text-red-dark rounded-md">
+                <Credits />
+            </main>
+        </div>
     )
 }
 
