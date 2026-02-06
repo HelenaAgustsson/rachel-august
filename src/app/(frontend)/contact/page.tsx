8483d87ebf68964fba334f4e0c0dfdfab6a5ef19
Helena Agustsson <agustssonhelena@gmail.com>
2026-02-06 11:22:19 +0000

    credit section bg

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
     return (
         <div>
             <StandardHero />
-            <main className="w-4/5 md:w-2/3 mx-auto mb-20 p-5 bg-orange-100 text-red-dark rounded-md">
+            <main className="w-4/5 md:w-2/3 mx-auto mb-20">
 
             </main>
         </div>
