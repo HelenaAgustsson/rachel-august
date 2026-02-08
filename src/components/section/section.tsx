5728f6c58c4849b51e039a1ce8bd3cc1dd6676ee
Helena Agustsson <agustssonhelena@gmail.com>
2026-02-08 17:00:50 +0000

    homepage section

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
 
 interface SectionProps {
-    children: ReactNode | ReactNode[];
+    children?: ReactNode | ReactNode[];
 }
 
 const Section = ({ children }: SectionProps) => {
     return (
-        <div className="py-10 px-10 lg:py-15 lg:px-20 bg-orange-100 text-red-dark rounded-md">
+        <div className="p-10 bg-orange-100 text-red-dark rounded-md">
             {children}
         </div>
     )
