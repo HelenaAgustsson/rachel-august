4b5c17f2b21135db116eaf7be4310be948a6321c
Helena Agustsson <agustssonhelena@gmail.com>
2026-02-08 16:49:17 +0000

    section component

--- src/components/credits/credits.tsx
+++ src/components/credits/credits.tsx
@@ -1,5 +1,6 @@
 
 import dayjs from "dayjs";
+import Section from "../section/section";
 import CreditLine from "./credit";
 import { VOICEOVER_QUERYResult, ACTING_QUERYResult } from "@/sanity/types";
 
@@ -16,9 +17,8 @@ export default async function Credits({ credits }: CreditProps) {
     ) : null
 
     return (
-        <div className="p-5 bg-orange-100 text-red-dark rounded-md">
-            <h3 className="text-xl lg:text-2xl font-bold">ACTING CREDITS</h3>
-            <div className="mt-10">
+        <div>
+            <div className="mt-15">
                 {credits ? credits.map((credit, index) => <CreditLine key={index} credit={credit} />) : null}
             </div>
         </div>
