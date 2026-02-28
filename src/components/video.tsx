fa9ccebb14cf01d9c972a5ab1bba7120be9ad335
Helena Agustsson <agustssonhelena@gmail.com>
2026-02-28 10:41:51 +0000

    styled section

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -4,7 +4,7 @@ type VideoProps = {
 
 const VideoComponent = ({ src }: VideoProps) => {
     return (
-        <div className="relative w-4/5 pt-[56.25%]">
+        <div className="relative w-full overflow-hidden pt-[56.25%]">
             <iframe className="absolute w-full h-full top-0 bottom-0 left-0 right-0" src={src} allowFullScreen />
         </div>
     )
